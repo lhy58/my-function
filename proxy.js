e16446cdb5a3bd2cd3bdcb836dea04d98fc6652a
dongjun <294551397@qq.com>
2019-02-01 16:15:44 +0800

    JSONP解决跨域问题

--- proxy.js
+++ proxy.js
@@ -18,3 +18,33 @@
 
 // 全局
 "proxy": "172.213.1.1:3001"
+
+
+// 九种跨域方式实现原理
+// 地址: https://juejin.im/post/5c23993de51d457b8c1f4ee1
+// 一.JSONP实现流程
+
+function jsonp({ url, params, callback }) {
+    return new Promise((resolve, reject) => {
+        let script = document.createElement('script')
+        window[callback] = function(data) {
+            resolve(data)
+            document.body.removeChild(script)
+        }
+        params = { ...params, callback } // wd=b&callback=show
+        let arrs = []
+        for (let key in params) {
+            arrs.push(`${key}=${params[key]}`)
+        }
+        script.src = `${url}?${arrs.join('&')}`
+        document.body.appendChild(script)
+    })
+}
+jsonp({
+    url: 'http://localhost:3000/say',
+    params: { wd: 'Iloveyou' },
+    callback: 'show'
+}).then(data => {
+    console.log(data)
+})
+
