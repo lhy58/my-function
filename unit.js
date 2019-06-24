1e410d27a77cc7f6b823d553350d944062d7b54e
汉营 <12463294551@qq.com>
2019-06-24 17:17:41 +0800

    时间处理方法

--- unit.js
+++ unit.js
@@ -118,3 +118,80 @@ function deepClone(obj){
     }
     return result
 }
+
+/*
+*深克隆
+*/
+function deepClone_1(obj){
+  if(Array.isArray(obj)){
+    return obj.map(item => {
+      return deepClone_1(item)
+    })
+  }else if(obj && typeof obj === 'object'){
+    const result = {}
+    Object.keys(obj).map(idx => {
+      result[idx] = deepClone_1(obj[idx])
+    })
+    return result
+  }
+  return obj
+}
+
+/*
+*比较两个日期相差的天数,可为负值
+*传入格式: 2018-12-18
+*/
+function difftime (time1, time2) {
+  let aDate1 = time1.split('-')
+  let aDate2 = time2.split('-')
+  let oDate1 = new Date(aDate1[0], aDate1[1] - 1, aDate1[2])
+  let oDate2 = new Date(aDate2[0], aDate2[1] - 1, aDate2[2])
+  let iDays = parseInt(Math.abs(oDate2 - oDate1) / (1000 * 3600 * 24))
+  if (oDate1 - oDate2 < 0){
+    return -iDays
+  }
+  return iDays
+}
+
+/*
+*传入时间格式:
+*2015-11-05 09:23:00
+*2015-11-05T09:23:00
+*2015/11/05 09:23:00
+*不兼容格式化20151105 09:23:00
+*返回日期显示逻辑如下:
+*1.当天日期显示时间
+*2.昨天和前天日期显示: 昨天 时间
+*3.更早日期显示: 月 日 时间
+*
+*/
+function parserTime (times) {
+  //把日期以:隔开,替换所有的: /,T,-为:
+  times = times.replace(/\//g, ':')
+  times = times.replace(/\-/g, ':')
+  times = times.replace(/\T/g, ':')
+  times = times.replace(' ', ':')
+  times = times.split(':')
+  let dd 
+  // 重新组合需比较日期
+  let inDate = times[0] + '-' + times[1] + '-' + tiems[2]
+  let inTime = times[3] + ':' + times[4]
+  // HH:MM:SS
+  let d = new Date()
+  let newDate = d.getFullYear()+ '-' + (d.getMonth() + 1) + '-' + d.getDate()
+  // 判断年份-今年
+  if (times[0] == d.getFullYear()){
+    if (difftime(newDate, inDate) == 0){
+      dd = inTime
+    } else if (difftime(newDate, inDate) == 1){
+      dd = '昨天 ' + inTime
+    } else if (difftime(newDate, inDate) == 2) {
+      dd = '前天 ' + inTime
+    } else {
+      dd = parseInt(items[1]) + '月' + parseInt(items[2]) + '日 ' + inTime
+    }
+  }else {
+    dd = times[0] + '/' + parseInt(items[1]) + '/' + parseInt(items[2]) + ' ' + inTime
+  }
+  return dd
+}
